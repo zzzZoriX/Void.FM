cc25b5218ecc0857c32de0613b594cc8d4a6003e
anyway <zorix228337@gmail.com>
2026-05-29 11:17:51 +0300

    init commit

---
+++ Void.FM/Assets/Script/Game/GameInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script.Game
+{
+    public class GameInitializer : MonoBehaviour
+    {
+        public Player Player;
+        
+        
+        private void Awake() {
+            Player.Initialize();
+        }
+
+        private void OnDestroy() {
+            Player.Deinitialize();
+        }
+    }
+}
---
+++ Void.FM/Assets/Script/Global/Common.cs
@@ -0,0 +1,36 @@
+﻿namespace Script.Global
+{
+    using UnityEngine;
+    
+    public class Common
+    {
+        public static Vector3 ConvertV2ToV3(Vector2 v2, float yPos)
+            => new(v2.x, yPos, v2.y);
+
+        public static void ConvertGlobalToLocal(out Vector3 local, Vector3 v3) {
+            var cameraRight = Camera.main.transform.right;
+            var cameraForward = Camera.main.transform.forward;
+
+            cameraForward.y = 0f;
+            cameraRight.y = 0f;
+
+            cameraForward.Normalize();
+            cameraRight.Normalize();
+
+            local = cameraForward * v3.z + cameraRight * v3.x;        
+        }
+
+        public static Vector3 ConvertGlobalToLocal(Vector3 v3) {
+            var cameraRight = Camera.main.transform.right;
+            var cameraForward = Camera.main.transform.forward;
+
+            cameraForward.y = 0f;
+            cameraRight.y = 0f;
+
+            cameraForward.Normalize();
+            cameraRight.Normalize();
+                
+            return cameraForward * v3.z + cameraRight * v3.x;        
+        }
+    }
+}
